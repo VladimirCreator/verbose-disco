d77fc8e923b23ded6ee3047fb403bbc2e30839df
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2024-03-08 03:04:06 +0000

    chore: update /next.config.mjs to support markdown

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 export default {
-
+	webpack: (config) =>{
+		config.module.rules.push({
+			test: /.md$/, type: 'asset/source'
+		})
+		return config
+	}
 }
